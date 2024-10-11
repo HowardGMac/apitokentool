f4f6cd1c3a83a2bdc1fc0b9dd19e5405a24a66f5
Howard Griffith <howardgriffith@yahoo.com>
2024-10-11 10:49:43 -0500

    Fix pathing in signing check of apitokentool.sh

--- client/apitokentool-bash/apitokentool.sh
+++ client/apitokentool-bash/apitokentool.sh
@@ -5,7 +5,8 @@ apiTokenKeyPreferenceDomain="{PREFERENCE DOMAIN}"
 
 # Get macOS computer serial, apitokentool signing info and apitokentool access key
 deviceSerial=$(ioreg -c IOPlatformExpertDevice -d 2 | awk -F\" '/IOPlatformSerialNumber/{print $(NF-1)}')
-apiTokenToolSigning=$(codesign -dv $0 2>&1|grep "TeamIdentifier=")
+apiTokenToolSource=${BASH_SOURCE[0]}
+apiTokenToolSigning=$(codesign -dv "$apiTokenToolSource" 2>&1|grep "TeamIdentifier=")
 apiTokenKey=$(defaults read "/Library/Managed Preferences/$apiTokenKeyPreferenceDomain.plist" apiTokenKey)
 
 curl -X POST -H "Content-Type: text/plain" --data "{\"serialNumber\": \"$deviceSerial\",\"signature\": \"${apiTokenToolSigning#*=}\",\"apiTokenKey\": \"$apiTokenKey\"}" $apiTokenToolURL
