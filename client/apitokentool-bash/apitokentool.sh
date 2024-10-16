7cc2e5f18d6a1fa48cfd2e5016870a9f92ddd37a
Luis Lugo <LuieLugo@gmail.com>
2024-10-16 09:58:11 -0400

    Update apitokentool.sh
    Change "${BASH_SOURCE[0]}" to "${BASH_SOURCE:-$0}" for wider environment support (sh, zsh, etc)

--- client/apitokentool-bash/apitokentool.sh
+++ client/apitokentool-bash/apitokentool.sh
@@ -5,7 +5,7 @@ apiTokenKeyPreferenceDomain="{PREFERENCE DOMAIN}"
 
 # Get macOS computer serial, apitokentool signing info and apitokentool access key
 deviceSerial=$(ioreg -c IOPlatformExpertDevice -d 2 | awk -F\" '/IOPlatformSerialNumber/{print $(NF-1)}')
-apiTokenToolSource=${BASH_SOURCE[0]}
+apiTokenToolSource=${BASH_SOURCE:-$0}
 apiTokenToolSigning=$(codesign -dv "$apiTokenToolSource" 2>&1|grep "TeamIdentifier=")
 apiTokenKey=$(defaults read "/Library/Managed Preferences/$apiTokenKeyPreferenceDomain.plist" apiTokenKey)
 
